9a362177b3c1b5a8c26eb67ed8c33ca4dcb9d93f
niole <niolenelson@gmail.com>
2024-07-20 09:31:05 -0700

    Add question input page

---
+++ src/routes/question_input/[id]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { writable } from 'svelte/store';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+	console.log(data);
+</script>
+
+<svelte:head>
+	<title>Answer Question</title>
+</svelte:head>
+
+<section>
+	<h1>
+	Answer
+	</h1>
+</section>
+
+<style>
+</style>
