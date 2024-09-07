4cc8166a84b8b6021057707333d87e384184c377
niole <niolenelson@gmail.com>
2024-09-07 14:58:34 -0700

    Always build with no cache

--- build_push_prod.sh
+++ build_push_prod.sh
@@ -6,7 +6,7 @@ TAG=${ARG1:=$hash}
 new_image="docker.io/niole/teacher_app:$TAG"
 
 npm run build
-docker build . -t docker.io/niole/teacher_app:$TAG && docker push docker.io/niole/teacher_app:$TAG
+docker build --no-cache . -t docker.io/niole/teacher_app:$TAG && docker push docker.io/niole/teacher_app:$TAG
 
 echo ""
 echo "New Tag: ${TAG}"
